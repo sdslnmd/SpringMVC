9aabf102c7179f97ef12295375574ed0451dc7d1
sunluning <sdslnmd@gmail.com>
2014-07-27 11:23:22 +0800

    test
    test

---
+++ src/test/java/com/mkyong/common/controller/MovieControllerTest.java
@@ -0,0 +1,11 @@
+package com.mkyong.common.controller;
+
+import static org.junit.Assert.*;
+
+public class MovieControllerTest {
+
+    @org.junit.Test
+    public void testGetMovie() throws Exception {
+
+    }
+}
